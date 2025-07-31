93abd18a4f5248c4fc653c84fc6a4ab59d058576
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2025-07-31 12:33:33 +0530

    Create number_pattern.py

---
+++ number_pattern.py
@@ -0,0 +1,12 @@
+n = int(input("enter numbver of rows: "))
+
+for i in range(n):
+    number = str(i+1) + " "
+    print(number*(i+1))
+    
+# output: n = 5
+# 1
+# 2 2 
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
