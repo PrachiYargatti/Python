b50c8cda0201a409e1907ed2cb118b02c6b6f335
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-03-13 00:26:50 +0530

    Create AscendingNumberTriangle.py

---
+++ AscendingNumberTriangle.py
@@ -0,0 +1,8 @@
+class solution:
+    def printAscendingNumberTriangle(self, n):
+        num = 1
+        for row in range(1,n+1):
+            for col in range(1,row+1):
+                print(num,end=' ')
+                num += 1 
+            print()
