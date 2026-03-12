83264d2b87560d4c816bb5ec90d686d1a42cd2df
Prachi Gajanan Yargatti <yaragattiprachi@gmail.com>
2026-03-13 00:28:51 +0530

    Create SquarePattern.py

---
+++ SquarePattern.py
@@ -0,0 +1,9 @@
+class solution:
+    def printSquarePattern(self, n):
+        #Write your code here...
+        # for row in range(1,n+1):
+        #     for col in range(1,n+1):
+        #         print('*',end=' ')
+        #     print()
+        for i in range(1,n+1):
+            print('* '*n)
